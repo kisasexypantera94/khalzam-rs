767bb047bc666b5f7f2366ae829788e701989153
kisasexypantera94 <green.grinya@gmail.com>
2019-03-24 22:36:02 +0300

    update example

--- examples/ex.rs
+++ examples/ex.rs
@@ -2,6 +2,7 @@ use futures;
 use futures::future::Future;
 use khalzam::db::pg::PostgresRepo;
 use std::fs;
+use std::io::Write;
 use std::sync::Arc;
 use tokio_threadpool::ThreadPool;
 
@@ -12,20 +13,34 @@ fn main() {
     let rt = ThreadPool::new();
     let resources = fs::read_dir("../resources").unwrap();
     for path in resources {
-        let repo = pgrepo.clone();
-        rt.spawn(futures::lazy(move || {
-            repo.add(path.unwrap().path().to_str().unwrap()).unwrap();
-            Ok(())
-        }));
+        if let Ok(path) = path {
+            let repo = pgrepo.clone();
+            rt.spawn(futures::lazy(move || {
+                let name = String::from(path.path().file_name().unwrap().to_str().unwrap());
+                let path = String::from(path.path().to_str().unwrap());
+                match repo.add(&path) {
+                    Ok(()) => {
+                        let stdout = std::io::stdout();
+                        let _ = writeln!(&mut stdout.lock(), "Added {}", name,);
+                    }
+                    Err(e) => println!("Can't add {}: {}", name, e),
+                }
+                Ok(())
+            }));
+        }
     }
+
+    rt.shutdown().wait().unwrap();
 
     let samples = fs::read_dir("../samples").unwrap();
     for path in samples {
-        let name = path.as_ref().unwrap();
-        print!("{:?} ", name);
-
-        println!("{:?}", pgrepo.recognize(name.path().to_str().unwrap()));
+        if let Ok(path) = path {
+            let name = String::from(path.path().file_name().unwrap().to_str().unwrap());
+            let path = String::from(path.path().to_str().unwrap());
+            println!("Recognizing `{}` ...", name);
+            println!("Best match: {}", pgrepo.recognize(&path).unwrap());
+        }
     }
 
-    rt.shutdown_on_idle().wait().unwrap();
+    println!("{}", pgrepo.delete("samka").unwrap());
 }
